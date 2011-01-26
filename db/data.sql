b9179f3fd19302f7c5fe2af5e51bd73cd36593e5
wooptoo <wooptoo@gmail.com>
2011-01-26 18:27:11 +0200

    added CFs

--- db/data.sql
+++ db/data.sql
@@ -14,23 +14,23 @@ insert into simptoms values(9, 'Strănut');
 insert into simptoms values(10, 'Frisoane');
 
 
-insert into rules values(null, 1, 1, cf);
-insert into rules values(null, 1, 5, cf);
-insert into rules values(null, 1, 6, cf);
-insert into rules values(null, 1, 8, cf);
-insert into rules values(null, 1, 9, cf);
-insert into rules values(null, 1, 10, cf);
+insert into rules values(null, 1, 1, 15);
+insert into rules values(null, 1, 5, 15);
+insert into rules values(null, 1, 6, 15);
+insert into rules values(null, 1, 8, 25);
+insert into rules values(null, 1, 9, 15);
+insert into rules values(null, 1, 10, 15);
 
-insert into rules values(null, 2, 2, cf);
-insert into rules values(null, 2, 3, cf);
-insert into rules values(null, 2, 5, cf);
-insert into rules values(null, 2, 7, cf);
-insert into rules values(null, 2, 9, cf);
-insert into rules values(null, 2, 10, cf);
+insert into rules values(null, 2, 2, 25);
+insert into rules values(null, 2, 3, 25);
+insert into rules values(null, 2, 5, 8);
+insert into rules values(null, 2, 7, 25);
+insert into rules values(null, 2, 9, 8);
+insert into rules values(null, 2, 10, 9);
 
-insert into rules values(null, 3, 1, cf);
-insert into rules values(null, 3, 4, cf);
-insert into rules values(null, 3, 5, cf);
-insert into rules values(null, 3, 6, cf);
-insert into rules values(null, 3, 9, cf);
-insert into rules values(null, 3, 10, cf);
+insert into rules values(null, 3, 1, 15);
+insert into rules values(null, 3, 4, 25);
+insert into rules values(null, 3, 5, 15);
+insert into rules values(null, 3, 6, 15);
+insert into rules values(null, 3, 9, 15);
+insert into rules values(null, 3, 10, 15);
