3d23fe4216cc6c3cb0c5b5887648d9aff573f856
wooptoo <wooptoo@gmail.com>
2011-01-26 20:08:54 +0200

    select, join tests

--- db.py
+++ db.py
@@ -1,5 +1,28 @@
 import web
 
-db = web.database(dbn='mysql', db='es', user='root', pw='')
+db = web.database(dbn='mysql', db='es', user='root')
 
-print db.select('symptoms')
+def select(table):
+    result = db.select(table)
+    return result.list()
+
+
+#~ symptoms = select('symptoms')
+#~ 
+#~ 
+#~ for row in symptoms:
+    #~ print row.name
+
+
+def join():
+    results = db.query(
+        'select diseases.name as dis, symptoms.name as symp from rules \
+        join diseases on rules.id_disease = diseases.id_disease \
+        join symptoms on rules.id_symptom = symptoms.id_symptom'
+    )
+    return results.list()
+    
+#~ x = join()
+#~ 
+#~ for row in x:
+    #~ print row
