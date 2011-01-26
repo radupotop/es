b30ebd451ce53c0ea99dc4373502143780b45ea4
wooptoo <wooptoo@gmail.com>
2011-01-26 17:26:44 +0200

    schema sql

--- schema.sql
+++ schema.sql
@@ -1,20 +1,32 @@
+drop table if exists simptoms;
+create table simptoms (
+    id_simptom int unsigned not null auto_increment,
+    primary key (id_simptom),
+    name varchar(255)
+);
+
+drop table if exists diseases;
+create table diseases (
+    id_disease int unsigned not null auto_increment,
+    primary key (id_disease),
+    name varchar(255)
+);
+
 drop table if exists rules;
 create table rules (
     id_rule int unsigned not null auto_increment,
     primary key (id_rule),
-    title varchar(255)
+    id_simptom int unsigned not null,
+    foreign key (id_simptom) references simptoms(id_simptom),
+    id_disease int unsigned not null,
+    foreign key (id_disease) references diseases(id_disease),
+    cf int unsigned not null
 );
 
-drop table if exists users;
-create table users (
-    id_user int unsigned not null auto_increment,
-    primary key (id_user),
-    id_group int unsigned not null,
-    foreign key (id_group) references groups(id_group),
-    first_name varchar(255),
-    fam_name varchar(255),
-    email varchar(255),
-    unique key (email),
-    pass varchar(255),
-    about text
+drop table if exists facts;
+create table facts (
+    id_fact int unsigned not null auto_increment,
+    primary key (id_fact),
+    id_rule int unsigned not null,
+    foreign key (id_rule) references rules(id_rule)
 );
